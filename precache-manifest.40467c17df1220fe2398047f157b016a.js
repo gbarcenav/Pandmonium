129db007bc504e1fe50d6ab802d8abff768dee32
MercedesDavila <mmdavila@outlook.com>
2019-09-17 22:28:50 -0500

    Updates

---
+++ precache-manifest.40467c17df1220fe2398047f157b016a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d84488d269265ac8d29939c3151b69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f06540cab336c3807662",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68a7dd0f72fc338953a5",
+    "url": "./static/js/2.e56924e7.chunk.js"
+  },
+  {
+    "revision": "f06540cab336c3807662",
+    "url": "./static/js/main.ca40df8d.chunk.js"
+  },
+  {
+    "revision": "da7c7cf1e315419d2d91",
+    "url": "./static/js/runtime~main.a1dc2674.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
