56882e83cb02de4065f1a858eb0156de84460cbf
BLANK-TECH <ivonne_1412@hotmail.com>
2019-09-19 18:38:10 -0500

    se agragan componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Inicio from './components/inicio';
+import PantallaOrden from './components/orden';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+  render(){
+    return (
+      <div className="App">
+      Aqui llamo a la pantalla de inicio: <Inicio/>
+      Aqui llamo a la pantalla de orden: <PantallaOrden />
+      </div>
+    )
+  }
 }
 
 export default App;
