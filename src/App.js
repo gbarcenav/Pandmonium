459b71a2ed3ace67be3dddfeb7273cbfbbc8270f
MercedesDavila <mmdavila@outlook.com>
2019-09-18 22:45:10 -0500

    Experimentando con menu hamburguesa

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
     
           <div id="page-wrap">
-            <h1>Cool Restaurant 🍔🍕</h1>
+          <span aria-label="emoji" role="img"> Cool Restaurant 🍔🍕</span>
             <h2>Check out our offerings in the sidebar!</h2>
           </div>
         </div>
