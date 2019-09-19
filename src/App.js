c0df054a88587f8e14bd805d93e4d46f46986670
MercedesDavila <mmdavila@outlook.com>
2019-09-18 22:26:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import SideBar from "./sidebar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="App">
+          <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+    
+          <div id="page-wrap">
+            <h1>Cool Restaurant 🍔🍕</h1>
+            <h2>Check out our offerings in the sidebar!</h2>
+          </div>
+        </div>
+      );
 }
 
 export default App;
