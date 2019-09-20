822277265f36b0299d82c60b508dfb9bb26587fc
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-09-20 05:44:50 -0500

    input ya es reutilizable y su placeholder es un elemento

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import element from './placeholder';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Input(props) {
+    return <input placeholder={props.placeholder}></input>
+}
+// const element = <Input placeholder="Ingresa tu código" />;
+  
+
+  export default Input;
