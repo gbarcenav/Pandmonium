0773a1ce19120d4d3197171943e8a49e64a2d515
Gabriela Barcena <g.barcena.v@gmail.com>
2019-09-20 17:15:34 -0500

    Se agrega prueba de sidebar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import Menu from "../components/ux_resources/menu-button.png";
 import Logo from "../components/ux_resources/Logo2017-02.png";
+import SideBar from "./sidebar";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <img src={Menu} className="menu-ham" alt="menú"></img>
+        <div id="Navbar">
+          <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+        </div>
         <img src={Logo} className="logo-nav" alt="logotipo Pandemonium"></img>
       </nav>
     );
