0b9005be92142b98e61aa5d762eae48d1cbbb8d5
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-09-21 18:10:03 -0500

    se agregaron etiquetas del menu en componente menu

--- src/components/pages/menu.js
+++ src/components/pages/menu.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Menuimages from '../menuimages.js';
 import Navbar from '../navbar';
+import TagFood from '../tagfood'
 
 
 class Menu extends Component {
@@ -9,7 +10,18 @@ class Menu extends Component {
           <section className="menu">
           <Navbar />
           <div className="All-menu">
+              <div className="menu-draws">
               <Menuimages/>
+              </div>
+              <div className="tag-ham">
+              <TagFood/>
+              <TagFood/>
+              <TagFood/>
+              <TagFood/>
+              <TagFood/>
+              <TagFood/>
+
+              </div>
 
           </div>
 
---
+++ src/components/tagfood.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Tagfood extends Component {
+    render() {
+      return (
+          <div className="tag-food">
+              
+          </div>
+      )
+    }
+}
+
+export default Tagfood;
