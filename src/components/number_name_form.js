b7f02d4aa74ca66bec7272bce8754fca4d8d6d68
MercedesDavila <mmdavila@outlook.com>
2019-09-21 22:31:59 -0500

    Se agregaron vistas y se aplico PWA

--- src/components/number_name_form.js
+++ src/components/number_name_form.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Input from "../components/input";
 import InputSpiner from "../components/input-spiner";
-import BtnGreen from "../components/btn_green";
+//import BtnGreen from "../components/btn_green";
 
 class FormNameNumber extends Component {
   render() {
