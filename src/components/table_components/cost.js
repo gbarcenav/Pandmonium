144b0c383c7a7bbae40f6eb9280ec64413fa100b
Gabriela Barcena <g.barcena.v@gmail.com>
2019-09-21 23:09:38 -0500

    Arreglamos el archivo para añadir una fila en la tabla de sección de Detalle de cuenta

--- src/components/table_components/cost.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class CostProduct extends Component {
-  render() {
-    return <td className="cost">{this.props.costQuantity}</td>;
-  }
-}
-
-export default CostProduct;
