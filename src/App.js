0264649a78780553dff455bd6418872a06bfcf8f
MercedesDavila <mmdavila@outlook.com>
2019-09-22 22:38:22 -0500

    Se publica app en Github pages

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import DetailPlace from "./components/pages/detail_place";
 import AccountList from "./components/pages/account_list";
 import Menu from "./components/pages/menu";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router basename={window.location.pathname || ''} >
+      
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/Places" component={Places} />
@@ -20,7 +21,7 @@ class App extends Component {
           <Route path="/Menu" component={Menu} />
           <Route path="/AccountList" component={AccountList} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
