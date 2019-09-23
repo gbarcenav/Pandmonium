927f8d6a8c9095caded0b86bdefaf785f13447f8
Gabriela Barcena <g.barcena.v@gmail.com>
2019-09-22 20:25:54 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
 // import './App.css';
 // import App from './App';
-import Menu from './components/pages/menu.js'
-import * as serviceWorker from './serviceWorker';
+import Menu from "./components/pages/menu.js";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Menu />, document.getElementById('root'));
-
+ReactDOM.render(<Menu />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
