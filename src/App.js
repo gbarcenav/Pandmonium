8fb946d4c00823ed42d0c474465a3d9f2a931816
BLANK-TECH <ivonne_1412@hotmail.com>
2019-09-23 10:41:15 -0500

    agregando PUBLIC

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter  basename = {process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/Places" component={Places} />
