3b225b736494bc38c4bfbf5737e95e83a89596ce
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-09-24 19:28:35 -0500

    Agregando carpeta node_modules a .gitignore

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -5,39 +5,6 @@ import logo from "../ux_resources/Logo2017-02.png";
 import CodeForm from "../code_form";
 
 class Login extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      empleado: {},
-      key: ''
-    };
-  }
-
-  componentDidMount() {
-    const ref = firebase.firestore().collection('empleado').doc(this.state.noEmpleado);
-    ref.get().then((doc) => {
-      if (doc.exists) {
-        this.setState({
-          empleado: doc.data(),
-          // key: doc.id,
-          isLoading: false
-        });
-      } else {
-        console.log("No such document!");
-      }
-    });
-  }
-
-  delete(id){
-    firebase.firestore().collection('usuario').doc(id).delete().then(() => {
-      console.log("Document successfully deleted!");
-      this.props.history.push("/")
-    }).catch((error) => {
-      console.error("Error removing document: ", error);
-    });
-  }
-
   render() {
     return (
       <div className="login-screen">
@@ -53,5 +20,3 @@ class Login extends Component {
     );
   }
 }
-
-export default Login;
