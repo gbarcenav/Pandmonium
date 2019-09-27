d8d0b5bee634ac0a4a96001488d237c15d6892c4
Gabriela Barcena <g.barcena.v@gmail.com>
2019-09-27 10:27:46 -0500

    Cambio de nombre de los archivos

--- src/components/Bars.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Bars extends Component {
-  render() {
-    return <button className="bars-btn">{this.props.number}</button>;
-  }
-}
-
-export default Bars;
