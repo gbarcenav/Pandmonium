c4f037461ce2a258aff0984efefb1b24539103e5
BLANK-TECH <ivonne_1412@hotmail.com>
2019-09-27 17:42:26 -0500

    cambio de nombre a archivo tables

--- src/components/Tables.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Tables extends Component {
-  render() {
-    return <button className="table-btn">{this.props.number}</button>;
-  }
-}
-
-export default Tables;
