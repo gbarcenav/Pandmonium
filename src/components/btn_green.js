ab4180d949b04b3c64b09bf131e73cc39e61dd36
BLANK-TECH <ivonne_1412@hotmail.com>
2019-09-28 22:57:46 -0500

    se guarda nombre del cliente en local storage

--- src/components/btn_green.js
+++ src/components/btn_green.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
 
 class BtnGreen extends Component {
+  
+
+  onClick = e =>{
+    e.preventDefault();
+    const {history} = this.props
+      localStorage.setItem('name', JSON.stringify(this.props.name));
+      history.push('/Menu')
+    }
+
   render() {
     return (
-      <button type="button" className="btn-green">
+      
+      <button 
+      type="button" 
+      className="btn-green"
+      onClick={this.onClick}>
         {this.props.btntext}
       </button>
+
     );
   }
 }
 
-export default BtnGreen;
+export default withRouter(BtnGreen);
--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 class Input extends Component {
+  
   render() {
     return (
       <input
         placeholder={this.props.placeholder}
         className={this.props.classCSS}
         type={this.props.typeInput}
+        onChange={this.props.onChange}
       ></input>
     );
   }
