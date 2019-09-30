7f827a4f40f8e6a212a896a99202e1a34fc84fbd
Gabriela Barcena <g.barcena.v@gmail.com>
2019-09-30 02:44:38 -0500

    Link de Places a Menu

--- src/components/number_name_form.js
+++ src/components/number_name_form.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import BtnGreen from "../components/btn_green";
+import { Link } from "react-router-dom";
+
 class FormNameNumber extends Component {
   constructor() {
     super();
@@ -29,25 +32,29 @@ class FormNameNumber extends Component {
 
   render() {
     return (
-      <form className={this.props.classCSS} onSubmit={this.handleSubmit}>
-        <label htmlFor="quantity">No. Personas</label>
-        <input
-          type="number"
-          name="quantity"
-          min="1"
-          className="input-spiner"
-          onChange={this.handleInput}
-        ></input>
-        <br></br>
-        <label htmlFor="diner">Nombre del cliente</label>
-        <input
-          name="diner"
-          className="input-name"
-          type="text"
-          onChange={this.handleInput}
-        ></input>
-        <button className="btn-green">ABRIR MESA</button>
-      </form>
+      <div>
+        <form className={this.props.classCSS} onSubmit={this.handleSubmit}>
+          <label htmlFor="quantity">No. Personas</label>
+          <input
+            type="number"
+            name="quantity"
+            min="1"
+            className="input-spiner"
+            onChange={this.handleInput}
+          ></input>
+          <br></br>
+          <label htmlFor="diner">Nombre del cliente</label>
+          <input
+            name="diner"
+            className="input-name"
+            type="text"
+            onChange={this.handleInput}
+          ></input>
+        </form>
+        <Link to="/Menu">
+          <BtnGreen btntext="ABRIR MESA" />
+        </Link>
+      </div>
     );
   }
 }
