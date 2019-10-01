069538376e35059bd64b81ae926eda41963a50f6
MercedesDavila <mmdavila@outlook.com>
2019-10-01 14:43:47 -0500

    Agregamos funcionalidad en botones de incremento y decremento

--- src/components/increase_decrease.js
+++ src/components/increase_decrease.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 class InputIncreaseDecrease extends Component {
+
+  state = {
+    counter: 0
+  }
+
+  Increase = () => {
+    if(this.state.counter >= 0){
+      this.setState({counter:this.state.counter + 1 })
+    }
+  }
+
+  Decrease = () => {
+    if(this.state.counter > 0){
+      this.setState({counter:this.state.counter - 1 })
+    }
+  }
+
   render() {
     return (
       <div className="form-inc-dec">
-        <button type="button" className="btn-inc-dec">
-          -
+        <button 
+        type="button" 
+        className="btn-inc-dec"
+        onClick={this.Decrease}>          -
         </button>
         <input type="number" className="pizza-input"></input>
         <button type="button" className="btn-inc-dec increase">
