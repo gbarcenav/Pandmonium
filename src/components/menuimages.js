0a6e84bc507df68614e8b03d1db9c96b0b0d73b5
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-01 16:52:03 -0500

    Class btn HD

--- src/components/menuimages.js
+++ src/components/menuimages.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 class FoodBtn extends Component {
   render() {
     return (
-      <button onClick={this.handleClick}>
-        <img src={this.props.image} alt={this.props.alt}></img>
+      <button onClick={this.handleClick} className="btn-product">
+        <img
+          src={this.props.image}
+          alt={this.props.alt}
+          className={this.props.styleCSS}
+        ></img>
       </button>
     );
   }
