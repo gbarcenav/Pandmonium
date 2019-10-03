df923fc44870095f2af1c06c15adb4af8afa56fa
Berenice <elcosmossuena@gmailcom>
2019-10-03 18:08:01 -0500

    se obtuvo el contenido del  select options con metodo map

--- src/components/pizza_menu.js
+++ src/components/pizza_menu.js
@@ -13,12 +13,25 @@ class PizzaMenu extends Component {
   }
 
   handleChange = (e) =>{
-    var newText = e.target.value;
-    console.log(newText);
+    if (e.target.name === 'select-type') {
+      this.setState({
+        valueType: e.target.value
+      })
+      
+    }else if (e.target.name === 'select-option') {
+      this.setState({
+        valueoption: e.target.value
+        
+        
+      });
+      console.log(this.state.valueoption);
+      
+    } 
   }
 
   render() {
     let nuevo = DataMenu[this.props.indice].tipo;
+    let detalle = DataMenu[this.props.indice].opcion;
 
     return (
       <table className="tag-menu-product">
@@ -32,7 +45,7 @@ class PizzaMenu extends Component {
         <tbody>
           <tr>
             <th>
-              <select className="options-menu" onChange={this.handleChange}>
+              <select className="options-menu" onChange={this.handleChange} name="select-type">
                 {nuevo.map((x, index) => {
                   return (
                     <option key={index} value={x.name}>
@@ -48,8 +61,14 @@ class PizzaMenu extends Component {
             </td>
 
             <th>
-              <select className="options-menu">
-                <option>Elige un tamaño</option>
+              <select className="options-menu" onChange={this.handleChange} name='select-option'>
+                {detalle.map((x, index)=>{
+                  return(
+                    <option key={index} value={x.name}>{x}</option>
+                  )
+                }
+                )}
+                
               </select>
             </th>
           </tr>
