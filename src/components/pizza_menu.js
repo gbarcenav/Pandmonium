e1378cf68f310746487551fe251fde119efba6f1
Berenice <elcosmossuena@gmailcom>
2019-10-06 16:40:36 -0500

    se guarda mesa, numero de personas, fecha y hora, falta que se agregue lo que se seleccione sin repeticiones

--- src/components/pizza_menu.js
+++ src/components/pizza_menu.js
@@ -18,79 +18,30 @@ class PizzaMenu extends Component {
   }
 
   
-  handleClick = (e) =>{
-    e.preventDefault()
-    if (e.target.name === 'select-type') {
-      this.setState({
-        valueType: e.target.value
-      })
-      
-    }else if (e.target.name === 'select-option') {
-      this.setState({
-        valueoption: e.target.value
-        
-        
-      });
-      console.log(this.state.valueoption);
-      
-    } 
+  componentDidMount = () =>{
     const currentDate = new Date();
     const db = firebase.firestore();
     const strDate = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`
     const strHour=`${currentDate.getHours()}-${currentDate.getMinutes()} hrs`
 
     db.collection('pedidos').add({
-  pedidos:"Pandemonium",
-  date: strDate,
-  dateHour: strHour,
-  name:localStorage.getItem('name'),
-  num:localStorage.getItem('num'),
-  option:this.state.valueoption,
-  orden:localStorage.getItem('order'),
-  table:localStorage.getItem('num-mesa')
-  
-  
-})
-.then((docRef) => {
-  console.log("Document written with ID: ", docRef.id);
-  
-})
-.catch((error) => {
-  console.error("Error adding document: ", error);
-}
-)
+    pedidos:"Pandemonium",
+    date: strDate,
+    dateHour: strHour,
+    name:localStorage.getItem('name'),
+    num:localStorage.getItem('num'),
+    table:localStorage.getItem('num-mesa')
     
+    })
+    .then((docRef) => {
+   console.log("Document written with ID: ", docRef.id);
+  
+   })
+   .catch((error) => {
+   console.error("Error adding document: ", error);
+   }
+   )  
   }
-  // componentDidMount(){
-  //   const currentDate = new Date();
-  //       const db = firebase.firestore();
-  //       const strDate = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`
-  //       const strHour=`${currentDate.getHours()}-${currentDate.getMinutes()} hrs`
-    
-  //       db.collection('pedidos').add({
-  //     pedidos:"Pandemonium",
-  //     date: strDate,
-  //     dateHour: strHour,
-  //     name:localStorage.getItem('name'),
-  //     num:localStorage.getItem('num'),
-  //     option:this.state.valueoption,
-  //     detalle:this.state.valueType,
-  //     table:localStorage.getItem('num-mesa')
-      
-      
-  //   })
-  //   .then((docRef) => {
-  //     console.log("Document written with ID: ", docRef.id);
-      
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error adding document: ", error);
-  //   }
-  //   )
-  // }
-
-  
-  
   render() {
     let nuevo = DataMenu[this.props.indice].tipo;
     let detalle = DataMenu[this.props.indice].opcion;
