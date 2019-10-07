d2717b3b0832f4c86018ca969e5dc9e5de603d07
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-06 22:13:38 -0500

    Modificación estilos

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -24,7 +24,7 @@ export default props => {
         <br />
         <hr></hr>
       </Link>
-      <Link>
+      <Link className="menu-item" to="/AccountList">
         Ventas del día
         <br />
         <br />
