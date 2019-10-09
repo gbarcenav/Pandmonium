394bdf0871ab77328023d875352351eafcc536b1
Gabriela Barcena <g.barcena.v@gmail.com>
2019-10-09 18:21:43 -0500

    Modifiqué estilos en tabla

--- src/components/pages/detail_place.js
+++ src/components/pages/detail_place.js
@@ -3,61 +3,63 @@ import React, { Component } from "react";
 import Navbar from "../navbar";
 import AccountPerGuest from "../account_guest";
 import GreenRectangle from "../green_rectangle";
-import * as firebase from 'firebase';
-
+import * as firebase from "firebase";
 
 class ShowPedido extends Component {
-  
-  render(){
-    return(
+  render() {
+    return (
       <div>
-
-      <div className="people-number-detail">
-      <span className="data-detail">Fecha: {this.props.date}</span><br></br>
-      <span className="data-detail">Hora: {this.props.dateHour}</span><br></br>
-    </div>
-       <div className="table-detail-container">
-         <AccountPerGuest completeArray={this.props.completeArray}/>
-       </div>
+        <div className="people-number-detail">
+          <span className="data-detail" style={{ fontWeight: 700 }}>
+            Fecha: {this.props.date}
+          </span>
+          <br></br>
+          <span className="data-detail" style={{ fontWeight: 700 }}>
+            Hora: {this.props.dateHour}
+          </span>
+          <br></br>
+        </div>
+        <div className="table-detail-container">
+          <AccountPerGuest completeArray={this.props.completeArray} />
+        </div>
       </div>
-
-    )
+    );
   }
 }
 
 class DetailPlace extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-      this.state = {
-        nametable: null,
-        date:null,
-        name:null,
-        completeArray:null
-
-    }
+    this.state = {
+      nametable: null,
+      date: null,
+      name: null,
+      completeArray: null
+    };
   }
 
-  componentWillMount = () => {   
-    const nametable = localStorage.getItem('nametable')
+  componentWillMount = () => {
+    const nametable = localStorage.getItem("nametable");
     this.setState({
-      nametable:nametable
-    })
-    const db = firebase.firestore(); 
-     const pedidosRef = db.collection('pedidos');   
-     pedidosRef.where('table', '==', nametable )   
-     .get()    
-    .then((onSnapshot) => {  
-         onSnapshot.forEach((doc) => {
+      nametable: nametable
+    });
+    const db = firebase.firestore();
+    const pedidosRef = db.collection("pedidos");
+    pedidosRef
+      .where("table", "==", nametable)
+      .get()
+      .then(onSnapshot => {
+        onSnapshot.forEach(doc => {
           const order = doc.data().completeArray;
           this.setState({
-            date:doc.data().date,
-            name:doc.data().name,
-            completeArray:order,
-            dateHour:doc.data().dateHour
-          })                 
-       })
-    })
-}
+            date: doc.data().date,
+            name: doc.data().name,
+            completeArray: order,
+            dateHour: doc.data().dateHour
+          });
+        });
+      });
+  };
   render() {
     return (
       <div className="detail_screen">
@@ -69,13 +71,12 @@ class DetailPlace extends Component {
           name={this.state.name}
         ></GreenRectangle>
 
-        <ShowPedido 
+        <ShowPedido
           date={this.state.date}
           name={this.state.name}
           completeArray={this.state.completeArray}
           dateHour={this.state.dateHour}
         ></ShowPedido>
-       
       </div>
     );
   }
--- src/components/table_detail.js
+++ src/components/table_detail.js
@@ -8,16 +8,11 @@ class TableDetail extends Component {
       <table>
         <thead>
           <tr>
-            <th>Concepto</th>
-            <th>Detalle</th>
-            <th>Cantidad</th>
-            <th>Precio</th>
+            <th>Concepto Detalle Cantidad</th>
           </tr>
         </thead>
         <tbody>
-          <AddRow
-            completeArray={this.props.completeArray}
-          />
+          <AddRow completeArray={this.props.completeArray} />
         </tbody>
       </table>
     );
